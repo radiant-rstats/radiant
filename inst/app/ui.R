ba68e48335fe245b177dad6cf5f433c9794c3c77
vnijs <radiant@rady.ucsd.edu>
2016-07-18 13:13:44 -0700

    use option for uis

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,6 +1,7 @@
 ## ui for design menu in radiant
 do.call(navbarPage,
-  c("Radiant", getOption("radiant.nav_ui"), design_ui, basics_ui, model_ui,
-    multivariate_ui, getOption("radiant.shared_ui"),
+  c("Radiant", getOption("radiant.nav_ui"), getOption("radiant.design_ui"),
+    getOption("radiant.basics_ui"), getOption("radiant.model_ui"),
+    getOption("radiant.multivariate_ui"), getOption("radiant.shared_ui"),
     help_menu("help_ui"))
 )
