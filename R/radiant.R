b926c908bc058f0443816aa84f36d8bff68b7fe1
vnijs <radiant@rady.ucsd.edu>
2016-07-21 17:05:59 -0700

    fix for lin_launcher

--- R/radiant.R
+++ R/radiant.R
@@ -142,12 +142,12 @@ lin_launcher <- function(app = c("radiant","radiant.data","radiant.design","radi
     Sys.chmod(fn1, mode = "0755")
 
     fn2 <- paste0(Sys.getenv("HOME"),"/Desktop/update_radiant.sh")
-    launch_string <- paste0("#!/usr/bin/env Rscript\nunlink('~/r_sessions/*.rds', force = TRUE)\ninstall.packages('radiant', repos = 'https://radiant-rstats.github.io/minicran/')\nsuppressWarnings(update.packages(lib.loc = .libPaths()[1], repos = 'https://radiant-rstats.github.io/minicran', ask = FALSE))\nsleep(1000)")
+    launch_string <- paste0("#!/usr/bin/env Rscript\nunlink('~/r_sessions/*.rds', force = TRUE)\ninstall.packages('radiant', repos = 'https://radiant-rstats.github.io/minicran/')\nsuppressWarnings(update.packages(lib.loc = .libPaths()[1], repos = 'https://radiant-rstats.github.io/minicran', ask = FALSE))\nSys.sleep(1000)")
     cat(launch_string, file = fn2, sep = "\n")
     Sys.chmod(fn2, mode = "0755")
 
     if (file.exists(fn1) && file.exists(fn2))
-      message("Done! Look for a file named radiant.sh on your desktop. Double-click it to start Radiant in your default browser. There is also a file called update_radiant.sh you can double click to update the version of Radiant on your computer.\n")
+      message("Done! Look for a file named radiant.sh on your desktop. Double-click it to start Radiant in your default browser. There is also a file called update_radiant.sh you can double click to update the version of Radiant on your computer.\n\nIf the .sh files are opened in a text editor when you double-click them go to File Manager > Edit > Preferences > Behavior and click 'Run executable text files when they are opened.")
     else
       message("Something went wrong. No shortcuts were created.")
 
