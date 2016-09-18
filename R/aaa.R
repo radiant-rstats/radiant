2188ed7d15cb4ea6f290b426d1ee79340d2361f2
vnijs <radiant@rady.ucsd.edu>
2016-09-18 12:25:23 -0700

    updates

--- R/aaa.R
+++ R/aaa.R
@@ -6,4 +6,5 @@ globalVariables(".rs.restartR")
 #' @docType package
 #' @import radiant.data radiant.design radiant.basics radiant.model radiant.multivariate shiny
 #' @importFrom import from
+#' @importFrom utils new.packages
 NULL
