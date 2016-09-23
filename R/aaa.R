b39fe2a9bf5c35abc513c63cc5b72ec59c759301
vnijs <radiant@rady.ucsd.edu>
2016-09-23 03:10:48 -0700

    import note

--- R/aaa.R
+++ R/aaa.R
@@ -5,6 +5,8 @@ globalVariables(".rs.restartR")
 #' @name radiant
 #' @docType package
 #' @import radiant.data radiant.design radiant.basics radiant.model radiant.multivariate shiny
+#' @importFrom psych corr.test KMO cortest.bartlett fa.sort principal
+#' @importFrom AlgDesign optFederov
 #' @importFrom import from
 #' @importFrom utils new.packages
 NULL
