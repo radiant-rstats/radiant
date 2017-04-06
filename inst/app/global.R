7d51ecdc07e62be4ed00dc527c69a51aeea85ade
vnijs <radiant@rady.ucsd.edu>
2017-04-05 18:08:15 -0700

    update

--- inst/app/global.R
+++ inst/app/global.R
@@ -17,6 +17,12 @@ addResourcePath("figures_basics", file.path(getOption("radiant.path.basics"),"ap
 addResourcePath("figures_model", file.path(getOption("radiant.path.model"),"app/tools/help/figures/"))
 addResourcePath("figures_multivariate", file.path(getOption("radiant.path.multivariate"),"app/tools/help/figures/"))
 
+## setting path for www resources
+addResourcePath("www_design", file.path(getOption("radiant.path.design"),"app/www/"))
+addResourcePath("www_basics", file.path(getOption("radiant.path.basics"),"app/www/"))
+addResourcePath("www_model", file.path(getOption("radiant.path.model"),"app/www/"))
+addResourcePath("www_multivariate", file.path(getOption("radiant.path.multivariate"),"app/www/"))
+
 ## loading url information
 source(file.path(getOption("radiant.path.design"), "app/init.R"), encoding = getOption("radiant.encoding"), local = TRUE)
 source(file.path(getOption("radiant.path.basics"), "app/init.R"), encoding = getOption("radiant.encoding"), local = TRUE)
