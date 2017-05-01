3e00812844ef7ecbeca19af2cbf23c0bd72b032d
vnijs <radiant@rady.ucsd.edu>
2017-05-01 15:20:03 -0700

    shinyapps

--- inst/app/for.shinyapps.io.R
+++ inst/app/for.shinyapps.io.R
@@ -2,12 +2,14 @@
 .rs.restartR()
 
 ## install the latest version from github so it will be used on shinyapps.io
-devtools::install_github("radiant-rstats/radiant.data")
+devtools::install_github("radiant-rstats/radiant.data", force = TRUE)
 devtools::install_github("radiant-rstats/radiant.design")
 devtools::install_github("radiant-rstats/radiant.basics")
 devtools::install_github("radiant-rstats/radiant.model")
 devtools::install_github("radiant-rstats/radiant.multivariate")
 devtools::install_github("radiant-rstats/radiant")
+
+# devtools::install_github("cran/Matrix")
 
 ## by listing the call to the radiant library it will get picked up as a dependency
 library(radiant)
