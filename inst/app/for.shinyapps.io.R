1555bf482087cc5d5de2f4d1e1b5558f765e92e3
vnijs <radiant@rady.ucsd.edu>
2017-11-09 09:48:10 -0800

    shinyapps.io

--- inst/app/for.shinyapps.io.R
+++ inst/app/for.shinyapps.io.R
@@ -8,6 +8,8 @@ devtools::install_github("radiant-rstats/radiant.basics", force = TRUE)
 devtools::install_github("radiant-rstats/radiant.model", force = TRUE)
 devtools::install_github("radiant-rstats/radiant.multivariate", force = TRUE)
 devtools::install_github("radiant-rstats/radiant", force = TRUE)
+# devtools::install_github("rstudio/rstudioapi", force = TRUE)
+# devtools::install_github("rstudio/shiny", force = TRUE)
 
 ## last pre-dplyr 0.7.1 release
 # devtools::install_github("radiant-rstats/radiant.data@v0.8.6", force = TRUE)
@@ -19,4 +21,4 @@ devtools::install_github("radiant-rstats/radiant", force = TRUE)
 # devtools::install_github("tidyverse/dplyr@v0.5.0")
 
 ## by listing the call to the radiant library it will get picked up as a dependency
-# library(radiant)
+library(radiant)
