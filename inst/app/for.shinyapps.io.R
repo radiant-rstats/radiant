9773bec97df869fc2fd1cc321701ccda9f3ecf72
vnijs <radiant@rady.ucsd.edu>
2017-11-28 22:39:13 -0800

    update

--- inst/app/for.shinyapps.io.R
+++ inst/app/for.shinyapps.io.R
@@ -1,5 +1,5 @@
 ## restart before installing from github
-# .rs.restartR()
+rstudioapi::restartSession()
 
 ## install the latest version from github so it will be used on shinyapps.io
 devtools::install_github("radiant-rstats/radiant.data", force = TRUE)
@@ -8,17 +8,8 @@ devtools::install_github("radiant-rstats/radiant.basics", force = TRUE)
 devtools::install_github("radiant-rstats/radiant.model", force = TRUE)
 devtools::install_github("radiant-rstats/radiant.multivariate", force = TRUE)
 devtools::install_github("radiant-rstats/radiant", force = TRUE)
-# devtools::install_github("rstudio/rstudioapi", force = TRUE)
-# devtools::install_github("rstudio/shiny", force = TRUE)
-
-## last pre-dplyr 0.7.1 release
-# devtools::install_github("radiant-rstats/radiant.data@v0.8.6", force = TRUE)
-# devtools::install_github("radiant-rstats/radiant.design@v0.8.1")
-# devtools::install_github("radiant-rstats/radiant.basics@v0.8.3")
-# devtools::install_github("radiant-rstats/radiant.model@v0.8.3")
-# devtools::install_github("radiant-rstats/radiant.multivariate@v0.8.2")
-# devtools::install_github("radiant-rstats/radiant@v0.8.2")
-# devtools::install_github("tidyverse/dplyr@v0.5.0")
+devtools::install_github("rstudio/rstudioapi", force = TRUE)
+devtools::install_github("rstudio/shiny", force = TRUE)
 
 ## by listing the call to the radiant library it will get picked up as a dependency
 library(radiant)
