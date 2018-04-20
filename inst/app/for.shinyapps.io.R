9c04521abe594fda960e21052f5e6cfbe71f0fdd
vnijs <radiant@rady.ucsd.edu>
2018-04-19 19:13:34 -0700

    MRB

--- inst/app/for.shinyapps.io.R
+++ inst/app/for.shinyapps.io.R
@@ -1,16 +1,18 @@
 ## restart before installing from github
-rstudioapi::restartSession()
+# rstudioapi::restartSession()
 
 ## install the latest version from github so it will be used on shinyapps.io
-devtools::install_github("radiant-rstats/radiant.data", force = TRUE)
-devtools::install_github("radiant-rstats/radiant.design", force = TRUE)
-devtools::install_github("radiant-rstats/radiant.basics", force = TRUE)
-devtools::install_github("radiant-rstats/radiant.model", force = TRUE)
-devtools::install_github("radiant-rstats/radiant.multivariate", force = TRUE)
-devtools::install_github("radiant-rstats/radiant", force = TRUE)
+packages <- c("radiant.data", "radiant.design", "radiant.basics", "radiant.model", "radiant.multivariate", "radiant")
+## Use the code below to install the development version
+# install.packages("devtools")
+sapply(
+  packages,
+  function(p) devtools::install_github(paste0("radiant-rstats/", p), ref = "MRB", dependencies = FALSE)
+)
 devtools::install_github("rstudio/rstudioapi", force = TRUE)
+devtools::install_github("rstudio/httpuv", force = TRUE)
 devtools::install_github("rstudio/shiny", force = TRUE)
-devtools::install_github("trestletech/shinyAce/shinyAce", force = TRUE)
+devtools::install_github("trestletech/shinyAce", force = TRUE)
 
 ## by listing the call to the radiant library it will get picked up as a dependency
 library(radiant)
