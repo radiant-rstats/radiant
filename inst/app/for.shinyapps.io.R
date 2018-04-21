b8125c1bf9f77a479d134a7cb8877a5e88119070
vnijs <radiant@rady.ucsd.edu>
2018-04-21 12:54:20 -0700

    MRB

--- inst/app/for.shinyapps.io.R
+++ inst/app/for.shinyapps.io.R
@@ -2,28 +2,46 @@
 # rstudioapi::restartSession()
 
 ## install the latest version from github so it will be used on shinyapps.io
-packages <- c("radiant.data", "radiant.design", "radiant.basics", "radiant.model", "radiant.multivariate", "radiant")
+packages <- c(
+  "radiant.data", "radiant.design", "radiant.basics",
+  "radiant.model", "radiant.multivariate", "radiant"
+)
 ## Use the code below to install the development version
-# install.packages("devtools")
+if (!require(devtools)) {
+  install.packages("devtools")
+}
 ret <- sapply(
   packages,
-  function(p) devtools::install_github(paste0("radiant-rstats/", p), ref = "MRB", dependencies = FALSE)
+  function(p) {
+    devtools::install_github(
+      paste0("radiant-rstats/", p),
+      ref = "MRB",
+      dependencies = FALSE
+    )
+  }
 )
 devtools::install_github("rstudio/rstudioapi", force = TRUE)
 devtools::install_github("rstudio/httpuv", force = TRUE)
 devtools::install_github("rstudio/shiny", force = TRUE)
 devtools::install_github("trestletech/shinyAce", force = TRUE)
 
+## to install the release version
 packages <- c(
   "radiant.data", "radiant.design", "radiant.basics",
   "radiant.model", "radiant.multivariate", "radiant",
   "shiny", "httpuv", "later", "rstudioapi"
 )
-
+# packages <- c(
+#   "radiant.data", "radiant.design", "radiant.basics",
+#   "radiant.model", "radiant.multivariate", "radiant"
+# )
 ret <- sapply(
   packages,
   install.packages,
-  repos = c("https://radiant-rstats.github.io/minicran/dev/", "https://radiant-rstats.github.io/minicran/")
+  repos = c(
+    "https://radiant-rstats.github.io/minicran/dev/",
+    "https://radiant-rstats.github.io/minicran/"
+  )
 )
 
 ## by listing the call to the radiant library it will get picked up as a dependency
