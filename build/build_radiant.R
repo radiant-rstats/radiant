8d895507828a38caf3d0f3c49ae83987deec02bd
vnijs <radiant@rady.ucsd.edu>
2019-09-12 19:50:48 -0700

    CRAN

--- build/build_radiant.R
+++ build/build_radiant.R
@@ -51,11 +51,11 @@ apps <- c(
   # "shinyFiles",
   # "gitgadget",
   "radiant.data",
-  "radiant.design",
-  "radiant.basics",
+  # "radiant.design",
+  # "radiant.basics",
   "radiant.model",
-  "radiant.multivariate",
-  "radiant.update",
+  # "radiant.multivariate",
+  # "radiant.update",
   "radiant"
 )
 
