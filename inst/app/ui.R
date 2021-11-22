0391b8c1ff930cabafde9ce210ad51dedd13cc41
vnijs <vnijs@ucsd.edu>
2021-11-22 05:24:11 +0000

    CRAN 1.4.1

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,16 +1,18 @@
 ## ui for radiant
 navbar_proj(
-  do.call(
-    navbarPage,
-    c(
-      "Radiant",
-      getOption("radiant.nav_ui"),
-      getOption("radiant.design_ui"),
-      getOption("radiant.basics_ui"),
-      getOption("radiant.model_ui"),
-      getOption("radiant.multivariate_ui"),
-      getOption("radiant.shared_ui"),
-      help_menu("help_ui")
+  suppressWarnings(
+    do.call(
+      navbarPage,
+      c(
+        "Radiant",
+        getOption("radiant.nav_ui"),
+        getOption("radiant.design_ui"),
+        getOption("radiant.basics_ui"),
+        getOption("radiant.model_ui"),
+        getOption("radiant.multivariate_ui"),
+        getOption("radiant.shared_ui"),
+        help_menu("help_ui")
+      )
     )
   )
 )
